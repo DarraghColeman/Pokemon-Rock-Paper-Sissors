7d89e55c48ca95a569e6e045a1c0e36adae011fc
DarraghColeman <darragh.coleman1@gmail.com>
2022-09-28 13:19:27 +0000

    Bugs in script, html and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,40 @@
 function game() {
     const actions = ['fire', 'grass', 'rock', 'ice', 'ground'];
     const userWinResults = ['firegrass', 'grassrock', 'rockice', 'iceground', 'groundfire', 'fireice', 'grassground', 'rockfire', 'icegrass', 'groundrock']
+    let userChoice = '';
+    let compChoice = '';
+    const userChoiceElement = document.querySelector('.user-choice');
+    const pickedElement = document.querySelector('.user-pick');
+    const userPickElement = document.querySelector('.picked');
+    const pcPickElement = document.querySelector('.pc-pick');
+    const resultElement = document.querySelector('.result');
+    const resultTitleElement = resultElement.querySelector('.title');
+
+
+
     window.addEventListener('load', () => {
-        let userChoice = '';
+
 
         document.querySelectorAll('.user-choice .game-card').forEach(card => {
             card.addEventListener('click', (ev) => {
                 userChoice = getUserChoice(ev.target);
-            console.log(userChoice);
-
+                compChoice = getComputerChoice();
+            
                 startGame();
             })
-        })
+        });
+
+        resultElement.querySelector('button').addEventListener('click', tryAgain);
 
     })
 
     function startGame() {
+        calculateWinner(userChoice, compChoice);
+        userChoiceElement.classList.add('hidden');
+        pickedElement.classList.remove('hidden');
+        clearResultBeforeAppend();
+        buildChoiceElement(true, userChoice);
+        buildChoiceElement(false, compChoice);
 
     }
 
@@ -33,11 +52,38 @@ function game() {
 
     function calculateWinner(user, comp) {
         if (user === comp) {
-            console.log('Tie!');
-        } else if () {
-            console.log('You win!');
+            resultTitleElement.innerText = 'Tie!';
+        } else if (getUserWinsStatus(user + comp)) {
+            resultTitleElement.innerText = 'You win!';
         } else {
-            console.log('You lose!');
+            resultTitleElement.innerText = 'You lose!';
         }
     }
+
+    function getUserWinsStatus(result) {
+        return userWinResults.some(winStr => winStr === result);
+    }
+
+    function buildChoiceElement(userElement, className) {
+        let el = document.createdElement('div');
+        el.classList = [`game-card ${className}`];
+        el.innerHTML = `<img src="assets/images/${className}.png" alt="${classame}">`
+        if (isItUserElement) {
+            userPickElement.append(el);
+        } else {
+            pcPickElement.append(el);
+        }
+    }
+
+    function tryAgain() {
+        userChoiceElement.classList.remove('hidden');
+        pickedElement.classList.add('hidden');
+    }
+
+    function clearResultBeforeAppend() {
+        userPickElement.innerHTML = '';
+        pcPickElement.innerHTML = '';
+    }
 }
+
+game();
