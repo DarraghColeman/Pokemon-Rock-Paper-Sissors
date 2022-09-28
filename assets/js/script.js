792af01174399d358af85da715f22029b066f160
DarraghColeman <darragh.coleman1@gmail.com>
2022-09-28 12:09:55 +0000

    Add script eventListener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-console.log("Connected!")
+window.addEventListener('load', () => {
+    let userChoice = '';
+
+    document.querySelectorAll('.user-choice .game-card').forEach(card => {
+        card.addEventListener('click', (ev) => {
+            userChoice = getUserChoice(ev.target);
+            startGame();
+        })
+    })
+
+})
+
+function startGame() {
+
+}
+
+function getUserChoice(target) {
+    console.log(target);
+}
